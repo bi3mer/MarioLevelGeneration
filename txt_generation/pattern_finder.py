3df1e8ccab3288223c1246b740121608d856179a
bi3mer <colan@comcast.net>
2018-08-08 21:49:32 -0700

    attempt 1 at creating the maps would take way too long, not suprised but had to try

---
+++ txt_generation/pattern_finder.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageDraw
+
+sample_map = '../mario_levels/png/1-1.png'
+step_size = 16
+
+
+print "this way will take forever, don't bother"
+
+
+im = Image.open(sample_map).convert("RGB")
+draw = ImageDraw.Draw(im) 
+
+width, height = im.size
+
+for h in range(8, height - step_size, step_size):
+	draw.line((0,h, width,h))
+
+for w in range(0, width, step_size):
+	draw.line((w,0, w,height))
+	im.show()
+	raw_input("click enter")
