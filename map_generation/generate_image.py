b9fb6de7099f7c8a9c760a600a2b3ee9b1b4b162
bi3mer <colan@comcast.net>
2018-08-12 14:24:21 -0700

    goomba images can now be displayed. - separted code up - added comments - prepped for next step of preprocessing a map for complex behavior - fixed bug with goombas being rotated 90 degrees on generated image

--- map_generation/generate_image.py
+++ map_generation/generate_image.py
@@ -4,20 +4,27 @@ import json
 STEP_SIZE = 16
 
 def build_json():
+	'''
+	read in the json file for the tile info and return the src portion
+	as the info section is only useful for people trying to learn how 
+	this code base will work	
+	'''
 	f = open('tile_info.json')
 	data = json.load(f)
 	f.close()
 
-	# remove info section which is only useful for newcomers
 	return data['src']
 
 def empty_default_image(im):
+	'''
+	Set every pixel in the image to have an alpha of zero
+	'''
 	w,h = im.size
 
 	pixels = []
 	for x in xrange(w):
 		for y in xrange(h):
-			pixels.append((0,0,0,255))
+			pixels.append((0,0,0,0))
 
 	im.putdata(pixels)
 
@@ -31,27 +38,52 @@ def draw_from_sprite_sheet(tile_sets, image, x0, y0, data):
 	# adjust coodrinates to image space
 	x0 *= STEP_SIZE
 	y0 *= STEP_SIZE
+	print x0,y0
 
 	# put pixels into image
-	for x in xrange(STEP_SIZE):
-		for y in xrange(STEP_SIZE):
-			print x0, y0
-			print image.size
-
+	for y in xrange(STEP_SIZE):
+		for x in xrange(STEP_SIZE):
 			pixel = tile_set.getpixel((xt + x, yt + y))
-			print pixel
-			image.putpixel((x0,y0), pixel)
-			y0 += 1
-		x0 += 1
+			image.putpixel((x0-x,y0+y), pixel)
 
-def convert_map(map_str, save_path=None):
+def pre_process_map(matrix):
+	'''
+	Take a map and convert it into something that can easily be converted
+	into an image. This mainly contains special cases like the hammer 
+	spring which takes up two tile spaces instead of 1. The map has two 
+	s characters and this will replace it the bottom with sb and the top
+	with st. draw_from_sprite_sheet will use the json which already has
+	the defined behavior for these special characters to properly fill 
+	out the image
+	'''
+	pass
+
+def create_tilesets(): 
+	'''
+	open all tilesets from their source png and return them as an array
+	that links up with the tileinfo json file for the types where:
+		1 = enemy
+		2 = environment
+		3 = items
+	'''
+
 	env_sprite_sheet = Image.open('../assets/env_tileset.png')
 	enemy_sprite_sheet = Image.open('../assets/enemy_tileset.png')
 	items_sprite_sheet = Image.open('../assets/items_objects_tileset.png')
-	tile_sets = [enemy_sprite_sheet, env_sprite_sheet, items_sprite_sheet]
+	return [enemy_sprite_sheet, env_sprite_sheet, items_sprite_sheet]
+
+def convert_map(map_str, display=True, save_path=None):
+	'''
+	convert a string of a mario map into an image. 
+
+	- Set save_path to a string for this function to save the image. 
+	- Set Display to Falseto not display the map on conversion completion
+	'''
+	tile_sets = create_tilesets()
 
 	data = build_json()
 	lvl_map = map_str.strip().split('\n')
+	pre_process_map(lvl_map)
 
 	len_column = len(lvl_map)
 	len_row = len(lvl_map[0])
@@ -59,15 +91,20 @@ def convert_map(map_str, save_path=None):
 	height = len_column * STEP_SIZE
 	width = len_row * STEP_SIZE
 
-	im = Image.new('RGB', (height, width))
+	im = Image.new('RGBA', (height, width))
 	empty_default_image(im)
 
-	for y in xrange(len_column):
-		for x in xrange(len_row):
-			if lvl_map[y][x] == 'g':
-				draw_from_sprite_sheet(tile_sets, im, x, y, data['g'])
-				
-	im.show()
+	for x in xrange(len_column):
+		for y in xrange(len_row):
+			tile = lvl_map[x][y]
+			if tile in data:
+				draw_from_sprite_sheet(tile_sets, im, x, len_row - y - 1, data[tile])
+
+	if display:
+		im.show()
+
+	if save_path != None and save_path != "":
+		im.save_path(save_path)
 
 if __name__ == '__main__':
 	f = open('../levels/cleaned_maps/1-1.map', 'r')
