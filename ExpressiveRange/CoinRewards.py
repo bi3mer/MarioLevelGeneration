f7beaabf719f3ad2c2c1d42a95f3fd626dc65d1b
bi3mer <colan@comcast.net>
2019-02-09 16:00:19 -0800

    CoinRewards expressive range

---
+++ ExpressiveRange/CoinRewards.py
@@ -0,0 +1,12 @@
+def coin_rewards(level_map):
+	'''
+	Given a map we want to find how often we can find a coin. We expect 
+	tehre to be at most 3 coins per a column. So the score of 1 will be
+	received if the map has 3 coins on every column. Anything less will
+	be coins_found / (number_of_columns * 3)
+	'''
+	coins_found = 0
+	for column in level_map:
+		coins_found += column.count('c')
+
+	return coins_found / float(len(level_map) * 3)
---
+++ ExpressiveRange/__init__.py
@@ -0,0 +1,5 @@
+from .CoinRewards import coin_rewards
+# from Verticality import verticality
+# from Linearity import linearity
+# from Enemies import enemies
+# from Rewards import rewards
