ee268b95cabe06f32967b71817e381445c1862b9
bi3mer <colan@comcast.net>
2019-04-18 17:38:34 -0500

    removed command line arguments from rl.py for a better life

---
+++ config/rl.py
@@ -0,0 +1,110 @@
+# Every variable has a default and a hard coded value. If you do not want to use
+# the hard code value then just set it to None and these default values that have
+# been hard coded below
+assessment_iterations = 10
+grammar_name = None
+max_iterations = 10
+grammar_size = None
+minimum_maps = 10
+seed = 0
+
+# One of these need to be set but None is a valid option for rewards you do not
+# want set. All of these values should either be None or between 0 and 1
+target_coin_reward = None
+target_enemies = None
+target_linearity = 1
+target_rewards = None
+
+# The following sets defaults and does basic error checking in the configuration
+# file when it is imported
+if grammar_size == None:
+    grammar_size = 4
+    print(f'Defaulted grammar_size to {grammar_size}.')
+elif not isinstance(grammar_size, int):
+    raise Exception('Grammar size must be of type int.')
+elif grammar_size <= 0 or grammar_size > 7:
+    raise Exception('Grammar size must be greater than 0 and less than 7.')
+
+if grammar_name == None:
+    grammar_name = 'DEFAULT_GRAMMAR_NAME.json'
+    print(f'Defaulted grammar_name to {grammar_name}.')
+elif not isinstance(grammar_name, str):
+    raise Exception('grammar_name must be of type string.')
+elif len(grammar_name) == 0:
+    raise Exception('grammar_name cannot be an empty string')
+elif grammar_name.endswith('.json') == False:
+    grammar_name += '.json'
+    print(f'modifed grammar_name to include ".json": {grammar_name}')
+
+if max_iterations == None:
+    max_iterations = 5000
+    print(f'defaulted max_iterations to {max_iterations}.')
+elif not isinstance(max_iterations, int):
+    raise Exception('max_iterations must be of type integer.')
+elif max_iterations <= 0:
+    raise Exception('max iterations must be greater than 0.')
+
+if seed == None:
+    from datetime import datetime
+    seed = datetime.now()
+    print(f'defaulted seed to {seed}.')
+elif not isinstance(seed, int):
+    raise Exception('seed must be of type int.')
+
+if minimum_maps == None:
+    minimum_maps = 10
+elif not isinstance(minimum_maps, int):
+    raise Exception('minimum_maps must be of type integer.')
+elif minimum_maps <= 0:
+    raise Exception('minimum_mpas must be greater than 0.')
+
+if assessment_iterations == None:
+    assessment_iterations = 100
+elif not isinstance(assessment_iterations, int):
+    raise Exception('assessment_iterations must be of type integer.')
+elif assessment_iterations <= 0:
+    raise Exception('assessment_iterations must be greater than 0.')
+
+
+# Now we check the rewards to make sure the user has input proper values and has
+# atleast one that is not None
+valid_target_configuration = False
+
+if target_coin_reward != None:
+    if not isinstance(target_coin_reward, float) and not isinstance(target_coin_reward, int):
+        raise Exception('target_coin_reward must be of either type float or int.')
+
+    if target_coin_reward < 0 or target_coin_reward > 1:
+        raise Exception('target coin_reward must be greater than or equal to 0 or less than or equal to 1')
+
+    valid_target_configuration = True
+
+if target_enemies != None:
+    if not isinstance(target_enemies, float) and not isinstance(target_enemies, int):
+        raise Exception('target_enemies must be of either type float or int.')
+
+    if target_enemies < 0 or target_enemies > 1:
+        raise Exception('target_enemies must be greater than or equal to 0 or less than or equal to 1')
+
+    valid_target_configuration = True
+
+if target_linearity != None:
+    if not isinstance(target_linearity, float) and not isinstance(target_linearity, int):
+        raise Exception('target_linearity must be of either type float or int.')
+
+    if target_linearity < 0 or target_linearity > 1:
+        raise Exception('target_linearity must be greater than or equal to 0 or less than or equal to 1')
+
+    valid_target_configuration = True
+
+if target_rewards != None:
+    if not isinstance(target_rewards, float) and not isinstance(target_rewards, int):
+        raise Exception('target_rewards must be of either type float or int.')
+
+    if target_rewards < 0 or target_rewards > 1:
+        raise Exception('target_rewards must be greater than or equal to 0 or less than or equal to 1')
+
+    valid_target_configuration = True
+
+if not valid_target_configuration:
+    raise Exception('Atleast one target heuristic must be set.')
--- rl.py
+++ rl.py
@@ -1,52 +1,21 @@
-from map_generation import generate_image
 from datetime import datetime
 from tqdm import tqdm
 
-import ExpressiveRange
-import GenerateMap
-import generator
-import argparse
-import Grammar
 import random
 import json
 import sys
 import os
 
-MIN_MAPS_FOUND = 10
+from map_generation import generate_image
 
-def build_arg_parser():
-	parser = argparse.ArgumentParser(description="Mario NGram Generation")
+import ExpressiveRange
+import GenerateMap
+import generator
+import Grammar
+import config
 
-	parser.add_argument('--grammar-size', type=int, help='REQUIRED: size of grammar being used', required=True)
-
-	parser.add_argument('--grammar-name', type=str, help='name of grammar to be stored')
-	parser.add_argument('--max-iterations', type=int, help='maximum number of iterations for rl process')
-	parser.add_argument('--seed', type=int, help='seed for rng')
-
-	help_text = '''
-		Number of iterations to be run when assessing the current status of the grammar that
-		the process is improving. I would recommend somewhere between 50 and a 100.
-		'''
-	parser.add_argument('--assessment-iterations', type=int, help=help_text)
-
-	parser.add_argument('--coin-rewards', type=float, help='target coin reward score for generated map, between 0 and 1')
-	parser.add_argument('--enemies', type=float, help='target enemy score for generated map, between 0 and 1')
-	parser.add_argument('--linearity', type=float, help='target linearity score for generated map, between 0 and 1')
-	parser.add_argument('--rewards', type=float, help='target rewards scroe for generated map, between 0 and 1')
-
-	return parser.parse_args()
-
-def validate_float_argument(expressive_type, val, targets):
-	if val == None:
-		return
-
-	if val < 0 or val > 1:
-		print(f'{expressive_type.name} must be between 0 and 1')
-		sys.exit(0)
-
-	targets[expressive_type] = val
-
-def rl(target_file_name, max_iterations, grammar_size, targets, assessment_iterations, seed):
+def rl(targets):
+	grammar_size = config.rl.grammar_size
 	grammars, index_to_column, column_to_index, weighted_grammars = generator.build_grammar_info(
 		grammar_size,
 		grammar_size)
@@ -55,7 +24,7 @@ def rl(target_file_name, max_iterations, grammar_size, targets, assessment_itera
 	grammar = grammars[grammar_size - 1]
 
 	print('Running RL to best match specified targets.')
-	for iteration in tqdm(range(max_iterations), ascii=True):
+	for iteration in tqdm(range(config.rl.max_iterations), ascii=True):
 		# initialize grammar targets with 0 for every heuristic used
 		average_evaluations = {}
 		for key in targets:
@@ -64,8 +33,8 @@ def rl(target_file_name, max_iterations, grammar_size, targets, assessment_itera
 		# Assess how the grammar we have is doing right now
 		weighted_grammar = Grammar.convert_counted_grammar_to_percentages(grammar, grammar_size, verbose=False)
 
-		for i in range(assessment_iterations):
-			map_grammar = GenerateMap.generate_map(weighted_grammar, seed, 50, False, start_index, flag_index, end_index)
+		for i in range(config.rl.assessment_iterations):
+			map_grammar = GenerateMap.generate_map(weighted_grammar, config.rl.seed, 50, False, start_index, flag_index, end_index)
 			map_text = GenerateMap.convert_grammar_array_to_map(map_grammar, index_to_column)
 			m = map_text.strip().split('\n')
 
@@ -75,7 +44,7 @@ def rl(target_file_name, max_iterations, grammar_size, targets, assessment_itera
 		min_eval_distance = {}
 		for key in average_evaluations:
 			# compute average for the given heuristic
-			average_evaluations[key] /= assessment_iterations
+			average_evaluations[key] /= config.rl.assessment_iterations
 
 			# convert the average to min distance from target for a map 
 			# to be utilized in updating the grammars count
@@ -87,7 +56,7 @@ def rl(target_file_name, max_iterations, grammar_size, targets, assessment_itera
 		maps = []
 		index = -1
 		broken = False
-		while len(maps) < MIN_MAPS_FOUND:
+		while len(maps) < config.rl.minimum_maps:
 			index += 1
 			if index > 10000:
 				print('\n\n\n')
@@ -130,56 +99,28 @@ def rl(target_file_name, max_iterations, grammar_size, targets, assessment_itera
 	grammar_info['index_to_column'] = index_to_column
 	grammar_info['column_to_index'] = column_to_index
 
-	path = os.path.join('grammars', target_file_name)
+	path = os.path.join('grammars', config.rl.grammar_name)
 	f = open(path, 'w')
 	f.write(json.dumps(grammar_info))
 	f.close()
-	print('completed. Run the following to see the results:')
-	print(f'python generator.py --grammar-file {path} --display-images')
+	
+	# helpful text
+	cmd = f'python generator.py --grammar-file {path} --display-images'
+	print('Run the following to see results:')
+	print(cmd)
+
+def set_target(expressive_type, val, targets):
+	if val == None:
+		return
+
+	targets[expressive_type] = val
 
 if __name__ == '__main__':
-	args = build_arg_parser()
-
 	targets = {}
-	validate_float_argument(ExpressiveRange.Type.CoinReward, args.coin_rewards, targets)
-	validate_float_argument(ExpressiveRange.Type.Enemies, args.enemies, targets)
-	validate_float_argument(ExpressiveRange.Type.Linearity, args.linearity, targets)
-	validate_float_argument(ExpressiveRange.Type.Rewards, args.rewards, targets)
+	set_target(ExpressiveRange.Type.CoinReward, config.rl.target_coin_reward, targets)
+	set_target(ExpressiveRange.Type.Enemies, config.rl.target_enemies, targets)
+	set_target(ExpressiveRange.Type.Linearity, config.rl.target_linearity, targets)
+	set_target(ExpressiveRange.Type.Rewards, config.rl.target_rewards, targets)
 
-	max_iterations = args.max_iterations
-	if max_iterations == None:
-		print('max iterations was not set. Defaulting value to 5000.')
-		max_iterations = 5000
-	elif max_iterations <= 0:
-		print('--max-iterations must be an integer greater than 0')
-		sys.exit(0)
-
-	target_file_name = args.grammar_name
-	if target_file_name == None:
-		target_file_name = datetime.now().strftime("%Y_%m_%d_%H_%M.json")
-		print(f'--grammar-name not set. Defaulting to "{target_file_name}".')
-	elif target_file_name.endswith('.json') == False:
-		print('--grammar-name must specify file name ending with ".json"')
-		sys.exit(0)
-
-	grammar_size = args.grammar_size
-	if grammar_size == None:
-		print('-grammar size not set. Defaulting to 5')
-		grammar_size = 5
-	elif grammar_size <= 0 or grammar_size >= 10:
-		print('--grammar-size must be greater than 0 and less than 10')
-		sys.exit(0)
-
-	assessment_iterations = args.assessment_iterations
-	if assessment_iterations == None:
-		print('--assessment-iterations not set. Defaulting to 25')
-		assessment_iterations = 25
-	elif assessment_iterations <= 0 or assessment_iterations >= 1000:
-		print('--assessment-iterations must be greater than 0 and less than 1000')
-		sys.exit(0)
-
-	seed = args.seed
-	if seed == None:
-		seed = datetime.now()
-	rl(target_file_name, max_iterations, grammar_size, targets, assessment_iterations, seed)
+	rl(targets)
 
